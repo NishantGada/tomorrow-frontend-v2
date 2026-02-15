0d6e957742f3d35ea161df9b84358b19cfb1b2e0
NishantGada <nishant.gada160@gmail.com>
2026-02-15 11:39:30 -0500

    creates custom hook to ensure UX

---
+++ src/hooks/useCachedResources.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import * as Font from 'expo-font';
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter';
+import { SpaceGrotesk_700Bold } from '@expo-google-fonts/space-grotesk';
+
+export default function useCachedResources() {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+  useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        await Font.loadAsync({
+          Inter_400Regular,
+          Inter_500Medium,
+          Inter_600SemiBold,
+          Inter_700Bold,
+          SpaceGrotesk_700Bold,
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  return isLoadingComplete;
+}
